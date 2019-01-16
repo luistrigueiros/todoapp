53dbecfe81134d75f2bccc2ab6ddc06657b454ce
Luis Trigueiros <luistrigueiros@gmail.com>
2019-01-16 01:13:41 +0000

    Incremental save

--- src/main/java/com/luist/todo/ui/LoginView.java
+++ src/main/java/com/luist/todo/ui/LoginView.java
@@ -2,6 +2,7 @@ package com.luist.todo.ui;
 
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("login")
@@ -9,5 +10,14 @@ public class LoginView extends VerticalLayout {
     public LoginView() {
         Label label = new Label("<<This is the login view>>");
         add(label);
+        add(buildUsername());
     }
+
+    private TextField buildUsername() {
+        TextField username = new TextField();
+        username.setLabel("Username");
+        username.setPlaceholder("placeholder text");
+        return username;
+    }
+
 }
