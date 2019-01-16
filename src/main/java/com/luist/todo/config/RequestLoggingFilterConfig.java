0d9de34765d0c9fbdea346d2619a4e702f1ab6af
Luis Trigueiros <ltrigueiros@cartrawler.com>
2019-01-16 18:23:04 +0000

    Incremental save

--- src/main/java/com/luist/todo/config/RequestLoggingFilterConfig.java
+++ src/main/java/com/luist/todo/config/RequestLoggingFilterConfig.java
@@ -1,13 +1,17 @@
 package com.luist.todo.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 public class RequestLoggingFilterConfig {
 
     @Bean
+    @ConditionalOnClass(value = {Filter.class})
     public CommonsRequestLoggingFilter logFilter() {
         CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
         filter.setIncludeQueryString(true);
